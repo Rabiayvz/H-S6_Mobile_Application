607b646deb579f3e4bb17782a38fa5c521f97a61
rabiayvz <rabiayvz38@gmail.com>
2025-04-14 18:09:15 +0300

    İlk commit

---
+++ lib/engel_tespit.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Sayfa 1")),
+      body: Center(child: Text("Burası Sayfa 1")),
+    );
+  }
+}
