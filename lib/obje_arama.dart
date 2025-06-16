f22a4668738cd07f28aa8f4efeb903c38b7bd3ca
Rabiayvz <rabiayvz38@gmail.com>
2025-06-16 18:30:16 +0300

    Update obje_arama.dart

--- lib/obje_arama.dart
+++ lib/obje_arama.dart
@@ -1,11 +1,148 @@
 import 'package:flutter/material.dart';
+import 'kayitli_obje_ekleme.dart';
+import 'genel_obje_arama.dart';
 
 class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Sayfa 1")),
-      body: Center(child: Text("Burası Sayfa 1")),
+      backgroundColor: Colors.white,
+      appBar: AppBar(backgroundColor: Colors.white, title: Text("Obje Arama")),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.white, Color(0xFFEBEBEB)],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Page4()),
+                  );
+                },
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.05),
+                        offset: Offset(0, 4),
+                        blurRadius: 30,
+                        spreadRadius: 2,
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons
+                              .create_outlined, // Buraya istediğin ikonu koyabilirsin
+                          size: 48,
+                          color: const Color.fromARGB(255, 160, 160, 160),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Kayıt Obje Ekleme', // Buraya başlık metni
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          'Bulmakta zorlanılan eşyaları bulmayı kolaylaştırma', // Açıklama metni
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Page5()),
+                  );
+                },
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.05),
+                        offset: Offset(0, 4),
+                        blurRadius: 30,
+                        spreadRadius: 2,
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons
+                              .find_in_page_outlined, // Buraya istediğin ikonu koyabilirsin
+                          size: 48,
+                          color: const Color.fromARGB(255, 160, 160, 160),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Genel Obje Arama', // Buraya başlık metni
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          'İstenilen objeyi çevre içinde arama', // Açıklama metni
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.transparent,
+        elevation: 0,
+      ),
     );
   }
 }
