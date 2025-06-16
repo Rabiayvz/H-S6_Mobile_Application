4cbb82e977c163c5cd9c4ac1cf76b690273ca032
Rabiayvz <rabiayvz38@gmail.com>
2025-06-16 18:29:29 +0300

    Add files via upload

---
+++ lib/genel_obje_arama.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Page5 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Hedef 2 Sayfa")),
+      body: Center(child: Text("Buradasın!")),
+    );
+  }
+}
---
+++ lib/kayitli_obje_ekleme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Page4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Hedef Sayfa")),
+      body: Center(child: Text("Buradasın!")),
+    );
+  }
+}
